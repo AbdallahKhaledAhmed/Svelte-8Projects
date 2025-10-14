d2fd6cb1b23da3d910828f29c1f80c78a4eddce6
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-10-14 13:36:13 +0300

    main structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,21 @@
 <script>
+	// @ts-nocheck
+
 	import HelloWorld from './components/HelloWorld.svelte';
+	import WelcomeWorld from './components/WelcomeWorld.svelte';
 	import Sidebar from './components/Sidebar.svelte';
-	const compArray = [{ name: 'HelloWorld', value: HelloWorld }];
+	const compArray = [
+		{ name: 'HelloWorld', value: HelloWorld },
+		{ name: 'WelcomeWorld', value: WelcomeWorld }
+	];
+	let toShow = $state(0);
+	function updateToShow(newVal) {
+		toShow = newVal;
+	}
 </script>
 
-<Sidebar comps={compArray} />
+{$inspect(toShow)}
+<Sidebar {updateToShow} {compArray} />
+<div class="flex justify-center items-center w-full min-h-[100vh]">
+	{@render compArray[toShow].value()}
+</div>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,32 +1,38 @@
 <script>
-	const { comps } = $props();
-	const show = $state(0);
-	const open = $state(false);
+	let { updateToShow, compArray } = $props();
+	let drawerOpen = $state(false);
 </script>
 
 <div class="drawer">
-	<input id="my-drawer-1" type="checkbox" class="drawer-toggle" />
+	<input id="my-drawer-1" type="checkbox" class="drawer-toggle" bind:checked={drawerOpen} />
 	<div class="drawer-content">
 		<!-- Page content here -->
 		<label
 			for="my-drawer-1"
-			class="btn drawer-button rounded-full font-bold text-xl absolute bottom-0"
-			>{open ? '<' : '>'}</label
+			class="drawer-button text-2xl w-8 h-8 fixed bottom-5 left-5 rounded-full font-bold cursor-pointer flex justify-center items-center"
 		>
+			>
+		</label>
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer-1" aria-label="close sidebar" class="drawer-overlay"></label>
 		<ol class="menu bg-base-200 min-h-full w-80 p-4">
 			<!-- Sidebar content here -->
-			<div class="flex items-center border-b-2 mb-5">
+			<div class="flex items-center border-b-2 mb-6">
 				<img src="/src/assets/256px-Svelte_Logo.svg.png" alt="Svelte Logo" class="w-15 m-5" />
 				<h1 class="text-4xl font-bold">8GUIs</h1>
 			</div>
-			{#each comps as comp, ind}
-				<li><button>{ind + 1}. {comp.name}</button></li>
+			{#each compArray as comp, ind (ind)}
+				<li class="my-1">
+					<button
+						class="font-semibold"
+						onclick={() => {
+							updateToShow(ind);
+							drawerOpen = !drawerOpen;
+						}}>{ind + 1}. {comp.name}</button
+					>
+				</li>
 			{/each}
 		</ol>
 	</div>
 </div>
-
-{@render comps[show].value()}
