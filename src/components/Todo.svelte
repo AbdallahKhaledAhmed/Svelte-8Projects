7d1605a826086e79fbb75603fb42b63f498cb9df
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-10-26 23:52:51 +0300

    Todo Structure

---
+++ src/components/Todo.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<div>
+	<h1>Tasks App</h1>
+	<label>
+		<p>Task</p>
+		<input type="text" />
+	</label>
+	<button class="btn">Add</button>
+
+	<p>1/2 tasks completed</p>
+	<div class="flex">
+		<button>All</button>
+		<button>Todo</button>
+		<button>Done</button>
+	</div>
+	<div class="">
+		<label for="">
+			<input type="checkbox" name="" id="" />
+			<p></p>
+		</label>
+		<button>Remove</button>
+	</div>
+</div>
